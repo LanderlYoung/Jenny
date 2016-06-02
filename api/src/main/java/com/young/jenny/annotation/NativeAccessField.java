fcaf302261a3b533d3c22624a34c5fced8f70efb
LanderlYoung <landerlyoung@gmail.com>
2016-06-02 20:53:21 +0800

    add nativeField reflace

---
+++ api/src/main/java/com/young/jenny/annotation/NativeAccessField.java
@@ -0,0 +1,10 @@
+package com.young.jenny.annotation;
+
+/**
+ * Author: taylorcyang@tencent.com
+ * Date:   2016-06-02
+ * Time:   20-52
+ * Life with Passion, Code with Creativity.
+ */
+public class NativeAccessField {
+}
