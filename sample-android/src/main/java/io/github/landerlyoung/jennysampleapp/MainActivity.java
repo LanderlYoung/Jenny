2110705025096e8876d7fcb148e507c992ea567b
landerlyoung <landerlyoung@gmail.com>
2020-02-03 16:06:36 +0800

    clean up demo

--- sample-android/src/main/java/io/github/landerlyoung/jennysampleapp/MainActivity.java
+++ sample-android/src/main/java/io/github/landerlyoung/jennysampleapp/MainActivity.java
@@ -18,8 +18,6 @@ package io.github.landerlyoung.jennysampleapp;
 import android.annotation.SuppressLint;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -116,28 +114,6 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
         mTextView.append("\n");
         mTextView.append(msg);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public void test(Callback callback) {
